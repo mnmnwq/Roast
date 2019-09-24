35422830f1b2941f8a21731aafa351ea835f9c60
mnmnwq <328435440@163.com>
2019-09-24 11:49:16 +0800

    aa

---
+++ app/Http/Controllers/Web/AuthenticationController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baiwei
+ * Date: 2019/9/24
+ * Time: 11:45 AM
+ */
+namespace App\Http\Controllers\Web;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthenticationController extends Controller{
+
+}
