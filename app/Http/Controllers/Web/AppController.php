d5a16308a93344e42263820b1a54a80d61c2bff4
mnmnwq <328435440@163.com>
2019-09-25 08:27:40 +0800

    aa

--- app/Http/Controllers/Web/AppController.php
+++ app/Http/Controllers/Web/AppController.php
@@ -7,5 +7,13 @@ use App\Http\Controllers\Controller;
 
 class AppController extends Controller
 {
-    //
+    public function getApp()
+    {
+        return view('app');
+    }
+
+    public function getLogin()
+    {
+        return view('login');
+    }
 }
