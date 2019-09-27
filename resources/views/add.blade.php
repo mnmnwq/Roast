f4d4833e3cbda853bdd7281475fb68ddfde234b8
mnmnwq <328435440@163.com>
2019-09-27 09:28:53 +0800

    aa

---
+++ resources/views/add.blade.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>题库系统</title>
+    </head>
+    <body>
+        <button style="background-color: #1b4b72">单选题</button>
+        <button >多选题</button>
+        <button >判断题</button>
+        <br/>
+        <table border="1">
+            <tr>
+                <td>题目ID</td>
+                <td>题目</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </body>
+</html>
